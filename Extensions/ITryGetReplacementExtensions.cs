8554e499c3fd611049f7e4c18b0e05d59368d948
anloffredo <anloffredo@massageenvy.com>
2020-09-15 09:10:35 -0700

    updates

---
+++ Extensions/ITryGetReplacementExtensions.cs
@@ -0,0 +1,47 @@
+﻿using Penguin.Api.Abstractions.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Penguin.Api.Abstractions.Extensions
+{
+    public static class ITryGetReplacementExtensions
+    {
+        public static string FindReplacement(this ITryGetReplacement source, string toReplace, IApiPlaylistSessionContainer Container)
+        {
+            if (source.TryFindReplacement(toReplace, Container, out object v))
+            {
+                return v.ToString();
+            }
+            else
+            {
+                throw new Exception($"Can not find replacement for '{toReplace}'");
+            }
+        }
+        public static bool TryFindReplacement(this ITryGetReplacement source, string toReplace, IApiPlaylistSessionContainer Container, out object v)
+        {
+            if (source is null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            if (toReplace is null)
+            {
+                throw new ArgumentNullException(nameof(toReplace));
+            }
+
+
+            foreach (string possibility in toReplace.Split(new string[] { " ?? " }, StringSplitOptions.None))
+            {
+                if (source.TryGetReplacement(possibility.Trim(), Container, out object newValue))
+                {
+                    v = newValue;
+                    return true;
+                }
+            }
+
+            v = null;
+            return false;
+        }
+    }
+}
---
+++ Interfaces/ITryGetReplacement.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Penguin.Api.Abstractions.Interfaces
+{
+    public interface ITryGetReplacement
+    {
+        bool TryGetReplacement(string toReplace, IApiPlaylistSessionContainer Container, out object v);
+    }
+}
